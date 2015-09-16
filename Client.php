9df69db7a3431b2925a63d00a2fe5ebe1e237422
AnatolyRugalev <anatoly.rugalev@gmail.com>
2015-09-16 12:37:02 +0500

    Added authentication and accept content-type

--- Client.php
+++ Client.php
@@ -5,6 +5,8 @@ namespace understeam\jira;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Stream\BufferStream;
 use understeam\httpclient\Event;
+use Yii;
+use yii\base\InvalidParamException;
 use yii\helpers\Json;
 
 /**
@@ -21,12 +23,15 @@ class Client extends \yii\base\Component
 
     public $jiraUrl;
 
+    public $username;
+    public $password;
+
     public $httpClientId = 'httpclient';
 
 
     public function getApiEndpointUrl()
     {
-        return rtrim($this->jiraUrl, '/') . '/jira/rest/api/2/';
+        return rtrim($this->jiraUrl, '/') . '/rest/api/2/';
     }
 
     public function getUrlOfPath($path)
@@ -34,8 +39,12 @@ class Client extends \yii\base\Component
         return $this->getApiEndpointUrl() . ltrim($path, '/');
     }
 
-    public function get($path)
+    public function get($path, $params = [])
     {
+        if (!empty($params)) {
+            $params = http_build_query($params);
+            $path .= '?' . $params;
+        }
         return $this->request('GET', $path);
     }
 
@@ -56,10 +65,14 @@ class Client extends \yii\base\Component
 
     public function request($method, $path, $body = [])
     {
+        $url = $this->getUrlOfPath($path);
         try {
-            $result = $this->httpClient->request($this->getUrlOfPath($path), $method, function (Event $event) use ($body) {
+            $result = $this->httpClient->request($url, $method, function (Event $event) use ($body) {
+                $request = $event->message;
+                $authString = base64_encode($this->username . ':' . $this->password);
+                $request->addHeader("Authorization", "Basic " . $authString);
+                $request->addHeader("Accept", "application/json");
                 if (!empty($body)) {
-                    $request = $event->message;
                     $body = new BufferStream();
                     if (is_array($body)) {
                         $body = Json::encode($body);
@@ -72,7 +85,8 @@ class Client extends \yii\base\Component
                 $result = Json::decode($result);
             }
         } catch (RequestException $e) {
-            $result = Json::decode($e->getResponse()->getBody()->__toString());
+            $string = $e->getResponse()->getBody()->__toString();
+            $result = Json::decode($string);
         }
         return $result;
     }

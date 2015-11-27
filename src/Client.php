759bbff81cacdc2a14af0cb083d69d0a687ddb60
Bennet Klarhölter <b.klarhoelter@eraffe-media.de>
2015-11-27 11:37:32 +0100

    Result handling

--- src/Client.php
+++ src/Client.php
@@ -21,102 +21,108 @@ use yii\helpers\Json;
 class Client extends Component
 {
 
-    public $jiraUrl;
+	public $jiraUrl;
 
-    public $username;
-    public $password;
+	public $username;
+	public $password;
 
-    public $httpClientId = 'httpclient';
+	public $httpClientId = 'httpclient';
 
-    public function getApiEndpointUrl()
-    {
-        return rtrim($this->jiraUrl, '/') . '/rest/api/2/';
-    }
+	public function getApiEndpointUrl()
+	{
+		return rtrim($this->jiraUrl, '/') . '/rest/api/2/';
+	}
 
-    public function getUrlOfPath($path)
-    {
-        return $this->getApiEndpointUrl() . ltrim($path, '/');
-    }
+	public function getUrlOfPath($path)
+	{
+		return $this->getApiEndpointUrl() . ltrim($path, '/');
+	}
 
-    public function get($path, $params = [])
-    {
-        if (!empty($params)) {
-            $params = http_build_query($params);
-            $path .= '?' . $params;
-        }
+	public function get($path, $params = [])
+	{
+		if (!empty($params)) {
+			$params = http_build_query($params);
+			$path .= '?' . $params;
+		}
 
-        return $this->request('GET', $path);
-    }
+		return $this->request('GET', $path);
+	}
 
-    public function post($path, $body = [])
-    {
-        return $this->request('POST', $path, $body);
-    }
+	public function post($path, $body = [])
+	{
+		return $this->request('POST', $path, $body);
+	}
 
-    public function delete($path, $body = [])
-    {
-        return $this->request('DELETE', $path, $body);
-    }
+	public function delete($path, $body = [])
+	{
+		return $this->request('DELETE', $path, $body);
+	}
 
-    public function put($path, $body = [])
-    {
-        return $this->request('PUT', $path, $body);
-    }
+	public function put($path, $body = [])
+	{
+		return $this->request('PUT', $path, $body);
+	}
 
-    public function getProject($key)
-    {
-        $data = $this->get("project/{$key}");
-        if (!isset($data['id'])) {
-            return null;
-        } else {
-            return Project::populate($this, $data);
-        }
-    }
+	public function getProject($key)
+	{
+		$data = $this->get("project/{$key}");
+		if (!isset($data['id'])) {
+			return null;
+		} else {
+			return Project::populate($this, $data);
+		}
+	}
 
-    public function request($method, $path, $body = [])
-    {
-        $url = $this->getUrlOfPath($path);
-        try {
-            $result = $this->httpClient->request($url, $method, function (Event $event) use ($body) {
-                $request = $event->message;
-                $authString = base64_encode($this->username . ':' . $this->password);
-                $request->addHeader("Authorization", "Basic " . $authString);
-                $request->addHeader("Accept", "application/json");
-                $request->addHeader("Content-Type", "application/json");
-                if (!empty($body)) {
-                    $stream = new BufferStream();
-                    if (is_array($body)) {
-                        $body = Json::encode($body);
-                    }
-                    $stream->write($body);
-                    $request->setBody($stream);
-                }
-            });
-            if (is_string($result)) {
-                $result = Json::decode($result);
-            }
-        } catch (RequestException $e) {
-            $string = $e->getResponse()->getBody()->__toString();
-            $result = Json::decode($string);
-        }
+	public function request($method, $path, $body = [])
+	{
+		$url = $this->getUrlOfPath($path);
+		try {
+			$result = $this->httpClient->request($url, $method, function (Event $event) use ($body) {
+					$request = $event->message;
+					$authString = base64_encode($this->username . ':' . $this->password);
+					$request->addHeader("Authorization", "Basic " . $authString);
+					$request->addHeader("Accept", "application/json");
+					$request->addHeader("Content-Type", "application/json");
+					if (!empty($body)) {
+						$stream = new BufferStream();
+						if (is_array($body)) {
+							$body = Json::encode($body);
+						}
+						$stream->write($body);
+						$request->setBody($stream);
+					}
+				});
+			if (is_string($result)) {
+				$result = Json::decode($result);
+			}
+		} catch (RequestException $e) {
 
-        return $result;
-    }
+			$result = $e->getResponse()->getBody()->__toString();
 
-    /**
-     * @return \understeam\httpclient\Client
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function getHttpClient()
-    {
-        return Yii::$app->get($this->httpClientId);
-    }
+			$contentType = $e->getResponse()->getHeader('Content-Type');
+			if (strpos($contentType, 'application/json') !== false)
+			{
+				$result = Json::decode($result);
+			}
+		}
 
-    public static function escapeValue($value) {
-        return strtr($value, [
-            '/' => '\u002f',
-            '.' => '\u002e',
-        ]);
-    }
+		return $result;
+	}
+
+	/**
+	 * @return \understeam\httpclient\Client
+	 * @throws \yii\base\InvalidConfigException
+	 */
+	public function getHttpClient()
+	{
+		return Yii::$app->get($this->httpClientId);
+	}
+
+	public static function escapeValue($value) {
+		return strtr($value, [
+				'/' => '\u002f',
+				'.' => '\u002e',
+			]);
+	}
 
 }
